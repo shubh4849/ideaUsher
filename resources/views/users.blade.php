2b8ef043b4abbc1c1166ab8c10e13b4021cdec69
shubh4849 <shubham.skhanna@gmail.com>
2023-05-19 03:20:18 +0530

    Aditions and removal

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -17,6 +17,9 @@
          tr.status-failed td:nth-child(4) {
          color: red;
          }
+         tr.status-No Email Sent td:nth-child(4) {
+         color: red;
+         }
       </style>
       <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
